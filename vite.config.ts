2efc3b3c231079c08c82f3815cdf435db746c251
Keisuke Miura <193408231+ksukemiura@users.noreply.github.com>
2026-05-25 18:14:40 +0900

    Revert "Update API target in Vite config to use environment variable"
    This reverts commit 7fc0549999fcd06ad5bd88c610e878c2c5f6c007.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/session': {
-        target: process.env.API_TARGET,
+        target: 'http://localhost:3000',
         changeOrigin: true,
       },
     },
